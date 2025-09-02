7806f14f5a6ca280a98b23b80cd2439cf93afb8c
ALA22min22 <alaminhossan4000@gmail.com>
2025-09-02 08:41:59 +0600

    history element is created and this portion is really heard my brain is stacked of to the few hourse right now alhamdulilah it is complate

--- js/dom.js
+++ js/dom.js
@@ -2,6 +2,7 @@
 
 // heart Portion :
 
+
 function increment(id){
     document.getElementById(id)
 .addEventListener('click', function(){
@@ -25,10 +26,16 @@ increment('heart-9');
 
 // call portion :
 
+// database :
+const history = [];
+
+
+
 function callFunction (service , number ){
     const coin = parseInt(document.getElementById('coin').innerText);
     if(coin < 20){
         alert('Coin is Short Plz Perches the coin')
+        return;
     }
     else{
         const calculate = coin - 20;
@@ -38,62 +45,126 @@ function callFunction (service , number ){
             
         document.getElementById('coin').innerText = calculate;
     }
+
+     const object = {
+        name : document.getElementById(service).innerText,
+        number : document.getElementById(number).innerText,
+        date : new Date().toLocaleTimeString(),
+    };
+    history.push(object)
+    
 }
+
+// function for history 
+function showHistory (){
+    document.getElementById('history-container').innerHTML = '';
+    const historyContainer = document.getElementById('history-container')
+    
+    for(const obj of history){
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="look-cart bg-[#fafafa] rounded-xl w-[328px] min-h-[50px] mx-auto mt-6 flex justify-between items-center p-2">
+                    <div>
+                        <h3 class="text-[18px] text-black font-medium">${obj.name}</h3>
+                        
+                        <p class="text-[#5c5c5c] mt-4 font-semibold">${obj.number}</p>
+                    </div>
+                    <div>
+                        <p class="text-[18px]">${obj.date}</p>
+                    </div>
+        `;
+        historyContainer.appendChild(div);
+    }
+}
+
 
 document.getElementById('call-1')
 .addEventListener('click', function(){
 
     callFunction('service-1','number-1');
-
-})
+    showHistory();    
+    
+});
 
 document.getElementById('call-2')
 .addEventListener('click', function(){
 
 callFunction('service-2','number-2');
 
+showHistory();
+
 })
 document.getElementById('call-3')
 .addEventListener('click', function(){
 
 callFunction('service-3','number-3');
+showHistory();
 
 })
 document.getElementById('call-4')
 .addEventListener('click', function(){
 
 callFunction('service-4','number-4');
+showHistory();
 
 })
 document.getElementById('call-5')
 .addEventListener('click', function(){
 
 callFunction('service-5','number-5');
+showHistory();
 
 })
 document.getElementById('call-6')
 .addEventListener('click', function(){
 
 callFunction('service-6','number-6');
+showHistory();
 
 })
 document.getElementById('call-7')
 .addEventListener('click', function(){
 
 callFunction('service-7','number-7');
+showHistory();
     
 })
 document.getElementById('call-8')
 .addEventListener('click', function(){
 
 callFunction('service-8','number-8');
+showHistory();
 
 })
 document.getElementById('call-9')
 .addEventListener('click', function(){
 
 callFunction('service-9','number-9');
+showHistory();
 
 })
+
+
+// copy portion :
+
+function copyText(copy) {
+    const copyText = document.getElementById(copy).innerText;
+    
+      const copyCount = parseInt(document.getElementById('copy-count').innerText)
+       
+        navigator.clipboard.writeText(copyText) 
+
+        if(copyText.innerText === copyText.innerText){
+            alert('Hotline number will be copied');
+        }
+
+         const count = copyCount + 1
+
+      document.getElementById('copy-count').innerText = count
+    
+}   
+
+// history portion :
+
 
 
